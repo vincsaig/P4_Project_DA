cec2b27137b3ca6780cb5b2ad2b03ceca4fdc008
vincsaig <90248988+vincsaig@users.noreply.github.com>
2021-10-10 23:14:53 +0200

    Added comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -6,17 +6,17 @@ import java.util.List;
 public class AnalyticsCounter {
 	public static void main(String args[]) throws Exception {
 		
+		//Asks the user to enter a path to the file
 		Scanner inputScanner = new Scanner(System.in);
-
 		System.out.println("Please enter the path to the file that will be read");
-
 		String filename = inputScanner.nextLine();
-
 		inputScanner.close();
 
 		ReadSymptomDataFromFile rSymptomsFromFile = new ReadSymptomDataFromFile(filename);
 
+		//Stores the raw content of the file in a List of String
 		List<String> listOfSymptoms = rSymptomsFromFile.GetSymptoms();
+		
 		// next generate output
 		/*FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
