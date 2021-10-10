98a18ecc4f1e24556b34ec41e53bb8059f0462ac
vincsaig <90248988+vincsaig@users.noreply.github.com>
2021-10-10 23:59:32 +0200

    Added an HashMap
    Added an HashMap that is used to remove any duplicates found in the given List of String

--- Project02Eclipse/src/com/hemebiotech/analytics/OrganizeListOfSymptoms.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/OrganizeListOfSymptoms.java
@@ -2,11 +2,19 @@ package com.hemebiotech.analytics;
 
 import java.util.TreeMap;
 import java.util.List;
-
+import java.util.HashMap;
+import java.util.Collections;
 public class OrganizeListOfSymptoms implements ListOrganizer{
 
     @Override
     public TreeMap<String, Integer> organizeListOfSymptoms(List<String> rawListOfSymptoms){
+        //Converting a List to a HashMap will remove any duplicates and allow
+        //us to count the elements found in the List
+        HashMap<String, Integer> symptomsAndNumberOfOccurences = new HashMap<>();
+        for(String symptom : rawListOfSymptoms){
+            symptomsAndNumberOfOccurences.put(symptom,Collections.frequency(rawListOfSymptoms, symptom));
+        }
+
         TreeMap<String, Integer> sortedSymptoms = new TreeMap<>();
         return sortedSymptoms;
 }
