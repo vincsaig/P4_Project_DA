e3ea77b980b42196c257c009e9fa61afc1ef6604
vincsaig <90248988+vincsaig@users.noreply.github.com>
2021-10-15 11:39:30 +0200

    Removed HashMap
    Removed HashMap from previous version as it serves no purpose and instead only use a TreeMap to sort and count occurences

--- Project02Eclipse/src/com/hemebiotech/analytics/OrganizeListOfSymptoms.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/OrganizeListOfSymptoms.java
@@ -2,7 +2,6 @@ package com.hemebiotech.analytics;
 
 import java.util.TreeMap;
 import java.util.List;
-import java.util.HashMap;
 import java.util.Collections;
 public class OrganizeListOfSymptoms implements ListOrganizer{
 
@@ -10,14 +9,11 @@ public class OrganizeListOfSymptoms implements ListOrganizer{
     public TreeMap<String, Integer> organizeListOfSymptoms(List<String> rawListOfSymptoms){
         //Using a HashMap to remove any duplicates found in the
         //given List
-        HashMap<String, Integer> symptomsAndNumberOfOccurences = new HashMap<>();
-        for(String symptom : rawListOfSymptoms){
-            symptomsAndNumberOfOccurences.put(symptom,Collections.frequency(rawListOfSymptoms, symptom));
-        }
 
-        //Using a TreeMap to organize all the elements in alphabetical order
         TreeMap<String, Integer> sortedSymptoms = new TreeMap<>();
-        sortedSymptoms.putAll(symptomsAndNumberOfOccurences);
+        for(String symptom : rawListOfSymptoms){
+            sortedSymptoms.put(symptom,Collections.frequency(rawListOfSymptoms, symptom));
+        }
         return sortedSymptoms;
 }
 }
