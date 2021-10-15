5f0c4b313fcb94b8b5fbb81af15f853d793a0fd9
vincsaig <90248988+vincsaig@users.noreply.github.com>
2021-10-15 12:22:30 +0200

    Added code to writerToOutputFile
    Added code to writerToOutputFile to handle output file generation

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteToOutputFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteToOutputFile.java
@@ -1,12 +1,31 @@
 package com.hemebiotech.analytics;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class WriteToOutputFile implements OSymptomWriter{
 
     @Override
-    public String writerToOutputFile(TreeMap<String, Integer> organizedListOfSymptoms){
+    public String writerToOutputFile(TreeMap<String,Integer> organizedListOfSymptoms){
 
+        if(!organizedListOfSymptoms.isEmpty()){
+            try(FileWriter oFile = new FileWriter("result.out")){
+                for(Entry<String, Integer> wLine : organizedListOfSymptoms.entrySet()){
+                    oFile.write(wLine.getKey() + " : " + wLine.getValue());
+                }
+                oFile.close();
+                return "File was generated successfully";
+
+            }   
+
+            catch(IOException e){
+                return "File was not generated successfully";
+            }
+        } else{
+            return "No Data in Map";
+        }
     }
 
 }
